2c0cdae646fb7d8dfe9ef4cfab2f3b81e5246738
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2022-04-11 12:02:53 +0300

    feat: add Location's class

---
+++ src/main/java/io/github/zektorum/data/Location.java
@@ -0,0 +1,7 @@
+package io.github.zektorum.data;
+
+public class Location {
+    private double x;
+    private Float y; //Поле не может быть null
+    private Double z; //Поле не может быть null
+}
