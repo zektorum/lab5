3235144fbd6b5995c53c1c8a3735a3cda6aad17d
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2022-04-11 12:07:39 +0300

    feat: add Location's constructor

--- src/main/java/io/github/zektorum/data/Location.java
+++ src/main/java/io/github/zektorum/data/Location.java
@@ -4,4 +4,10 @@ public class Location {
     private double x;
     private Float y; //Поле не может быть null
     private Double z; //Поле не может быть null
+
+    public Location(double x, float y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 }
