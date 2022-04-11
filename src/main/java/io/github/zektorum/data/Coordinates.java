79bc7a15b6229cf82c5f321de67e9b62d3fed0be
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2022-04-11 13:03:18 +0300

    feat: add Coordinates' class with fields and a constructor

---
+++ src/main/java/io/github/zektorum/data/Coordinates.java
@@ -0,0 +1,14 @@
+package io.github.zektorum.data;
+
+public class Coordinates {
+    private double x; //Значение поля должно быть больше -183
+    private Integer y; //Максимальное значение поля: 750, Поле не может быть null
+
+    public Coordinates(double x, int y) {
+        if (y > 750 || x < -183) {
+            // TODO: throw exception
+        }
+        this.x = x;
+        this.y = y;
+    }
+}
