5bb41c3060d61c1b604383579cef5f32a9195dec
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2022-04-11 13:00:18 +0300

    feat: add coordinates' field

--- src/main/java/io/github/zektorum/data/Person.java
+++ src/main/java/io/github/zektorum/data/Person.java
@@ -3,6 +3,7 @@ package io.github.zektorum.data;
 public class Person {
     private int id; //Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
+    private Coordinates coordinates; //Поле не может быть null
     private double height; //Значение поля должно быть больше 0
     private Location location; //Поле может быть null
 
