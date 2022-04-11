8d0b37a83c99b2c154677f59df92cc795deed7c8
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2022-04-11 12:05:51 +0300

    feat: add location field in Person and constructor overloading

--- src/main/java/io/github/zektorum/data/Person.java
+++ src/main/java/io/github/zektorum/data/Person.java
@@ -4,10 +4,16 @@ public class Person {
     private int id; //Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
     private double height; //Значение поля должно быть больше 0
+    private Location location; //Поле может быть null
 
     public Person(String name, double height) {
         this.name = name;
         this.height = height;
+    }
+
+    public Person(String name, double height, Location location) {
+        this(name, height);
+        this.location = location;
     }
 
     public double getHeight() {
