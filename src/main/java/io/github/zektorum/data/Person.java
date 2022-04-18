c6d350587d88057e022ddee85c1a087a3f5115d0
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2022-04-18 13:14:11 +0300

    feat: add coordinates field, remove constructor with two arguments, add coordinates argument in constructor

--- src/main/java/io/github/zektorum/data/Person.java
+++ src/main/java/io/github/zektorum/data/Person.java
@@ -3,17 +3,15 @@ package io.github.zektorum.data;
 public class Person {
     private int id; //Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
+    private Coordinates coordinates; //Поле не может быть null
     private double height; //Значение поля должно быть больше 0
     private Location location; //Поле может быть null
 
-    public Person(String name, double height) {
+    public Person(String name, double height, Location location, Coordinates coordinates) {
         this.name = name;
         this.height = height;
-    }
-
-    public Person(String name, double height, Location location) {
-        this(name, height);
         this.location = location;
+        this.coordinates = coordinates;
     }
 
     public double getHeight() {
