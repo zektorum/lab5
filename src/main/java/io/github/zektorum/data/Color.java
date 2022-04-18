075c841a27dee6f718e75cf770bee4e120999350
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2022-04-18 13:59:19 +0300

    feat: add class Color

---
+++ src/main/java/io/github/zektorum/data/Color.java
@@ -0,0 +1,17 @@
+package io.github.zektorum.data;
+
+public class Color {
+    public enum HairColor {
+        GREEN,
+        BLUE,
+        WHITE;
+    }
+
+    public enum EyeColor {
+        RED,
+        BLACK,
+        BLUE,
+        ORANGE,
+        BROWN;
+    }
+}
