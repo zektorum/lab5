67f32564619833f8b88e713cd4505f1ed421fc50
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2022-05-03 14:43:12 +0300

    feat: add handling systax error in json

--- src/main/java/io/github/zektorum/io/JsonReader.java
+++ src/main/java/io/github/zektorum/io/JsonReader.java
@@ -1,10 +1,20 @@
 package io.github.zektorum.io;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import io.github.zektorum.data.Person;
 
 public class JsonReader implements SerializableFormatReader {
     public Person[] getStructuredData(String content) {
-        return new Gson().fromJson(content, Person[].class);
+        Gson gson = new Gson();
+        Person[] people = new Person[]{};
+        try {
+            people = gson.fromJson(content, Person[].class);
+        } catch (JsonSyntaxException e) {
+            System.out.println("Ошибка парсинга Json! Некорректный синтаксис");
+            System.out.println("Завершение работы программы...");
+            System.exit(-11);
+        }
+        return people;
     }
 }
