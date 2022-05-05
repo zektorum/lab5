905cd8ba348350315938c66e77ba27bd323c1055
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2022-05-05 23:22:34 +0300

    fix: incorrect argument processing in InsertCommand

--- src/main/java/io/github/zektorum/command/InsertCommand.java
+++ src/main/java/io/github/zektorum/command/InsertCommand.java
@@ -6,13 +6,21 @@ public class InsertCommand extends ParentCommand {
     public InsertCommand() {
         super(
                 "insert",
-                "insert key",
+                "insert key {element}",
                 "добавить новый элемент с заданным ключом"
         );
     }
 
-    public void execute(PeopleCollection peopleCollection, String arg1, String arg2, String arg3) throws NumberFormatException {
-        PersonCreator pc = new PersonCreator(arg1, Integer.parseInt(arg2));
-        peopleCollection.insertElement(pc.create());
+    public void execute(PeopleCollection peopleCollection,
+                        String arg1, String arg2, String arg3) throws NumberFormatException {
+        try {
+            Integer.parseInt(arg1);
+            Integer.parseInt(arg3);
+        } catch (NumberFormatException e) {
+            System.out.println("Некорректные аргументы!\n");
+            return;
+        }
+        PersonCreator pc = new PersonCreator(arg2, Integer.parseInt(arg3));
+        peopleCollection.getPeopleCollection().put(Integer.parseInt(arg1), pc.create());
     }
 }
--- src/main/java/io/github/zektorum/data/Color.java
+++ src/main/java/io/github/zektorum/data/Color.java
@@ -2,16 +2,31 @@ package io.github.zektorum.data;
 
 public class Color {
     public enum HairColor {
-        GREEN,
-        BLUE,
-        WHITE;
+        GREEN(0), BLUE(1), WHITE(2);
+
+        private int value;
+
+        HairColor(int value){
+            this.value = value;
+        }
+
+        public int getValue() {
+            return this.value;
+        }
     }
 
     public enum EyeColor {
-        RED,
-        BLACK,
-        BLUE,
-        ORANGE,
-        BROWN;
+        RED(0), BLACK(1), BLUE(2),
+        ORANGE(3), BROWN(4);
+
+        private int value;
+
+        EyeColor(int value) {
+            this.value = value;
+        }
+
+        int getValue() {
+            return this.value;
+        }
     }
 }
