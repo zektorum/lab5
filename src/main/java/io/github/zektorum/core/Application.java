e99f6069d314ce7aea88574aca413436ff9997e6
muradmurtuzaliev <mur.murtuzaliev@yandex.ru>
2022-05-10 16:24:34 +0300

    feat: add permissions processing

--- src/main/java/io/github/zektorum/core/Application.java
+++ src/main/java/io/github/zektorum/core/Application.java
@@ -5,6 +5,7 @@ import io.github.zektorum.data.Person;
 import io.github.zektorum.io.FileReader;
 import io.github.zektorum.io.SerializableReader;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Application {
@@ -20,6 +21,13 @@ public class Application {
             if (filename == null) {
                 System.out.println("Некорректное значение переменной среды!");
                 System.exit(8);
+            }
+            if (!new File(filename).canRead()) {
+                System.out.println("Отсутствуют права на чтение!\nЗавершение работы...");
+                System.exit(3);
+            } else if (!new File(filename).canWrite()) {
+                System.out.println("Отсутствуют права на запись!\nЗавершение работы...");
+                System.exit(4);
             }
             FileReader fileReader = new FileReader(filename);
             Person[] structuredData = fileReader.read(new SerializableReader());
